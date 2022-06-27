a3c65a61939f6e49c1837e04dfa0d789a8c6cc82
Vinícius Kauã <103951509+viniznn777@users.noreply.github.com>
2022-06-26 22:46:01 -0300

    commit

---
+++ SaibaMais.js
@@ -0,0 +1,7 @@
+let aSaiba = document.getElementById('saibamais')
+let contentseemore = document.getElementById('seemore')
+
+function veja() {
+    aSaiba.style.display = 'none'
+    contentseemore.style.display = 'block'
+}
